d02549238cb819886a8d6d71564f65d9410fb5ea
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-06-08 09:09:01 +0700

    Implement InputNumber for NumericInput

--- src/shared/antd/numericInput.tsx
+++ src/shared/antd/numericInput.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useState, forwardRef, useCallback, useRef } from 'react'
+import { useState, forwardRef, useCallback } from 'react'
 
-import { Input, Tooltip, Space, InputProps } from 'antd'
+import { Tooltip, Space, InputNumber, InputNumberProps } from 'antd'
 import IonIcon from '@sentre/antd-ionicon'
 
 let timeoutId: ReturnType<typeof setTimeout> | undefined
@@ -20,19 +20,17 @@ const NumericInput = forwardRef(
       onValue = () => {},
       onChange = () => {},
       ...props
-    }: InputProps & {
-      onValue?: (val: string) => void
+    }: InputNumberProps & {
+      onValue?: (val: number | null) => void
       max?: string | number
     },
     ref: any,
   ) => {
     const [error, setError] = useState('')
-    const [cursor, setCursor] = useState<number | null>(null)
-    const innerRef = useRef(ref)
 
     // Handle amount
     const onAmount = useCallback(
-      (val: string) => {
+      (val: number) => {
         const onError = (er: string) => {
           if (timeoutId) {
             clearTimeout(timeoutId)
@@ -41,18 +39,12 @@ const NumericInput = forwardRef(
           setError(er)
           timeoutId = setTimeout(() => setError(''), 500)
         }
-        const reg = /^\d*(\.\d*)?$/
-        if (!reg.test(val)) return onError('Invalid character')
-        if (max && parseFloat(val) > parseFloat(max.toString()))
+        if (max && val > parseFloat(max.toString()))
           return onError('Not enough balance')
         return onValue(val)
       },
       [max, onValue],
     )
-
-    // Handle cursor jumping
-    // To prevent autofocus on mobile, we must strictly check cursor different from null
-    if (cursor !== null) innerRef?.current?.setSelectionRange(cursor, cursor)
 
     return (
       <Tooltip
@@ -64,14 +56,14 @@ const NumericInput = forwardRef(
         }
         visible={!!error}
       >
-        <Input
+        <InputNumber
           {...props}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            onChange(e)
-            setCursor(e.target.selectionStart)
-            onAmount(e.target.value || '')
+          type="number"
+          controls={false}
+          onChange={(e: string | number | null) => {
+            if (e === null || typeof e === 'string') return onValue(null)
+            onAmount(e)
           }}
-          ref={innerRef}
         />
       </Tooltip>
     )
