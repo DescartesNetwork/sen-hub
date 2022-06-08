cfa7aa1483bb5a6379f5e5eb7e946f2ba56b61b9
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-06-08 16:39:28 +0700

    use Map to alter for Array in useJupiterTokenList

--- src/shared/antd/mint/mintSelection/hooks/useJupiterTokenList.ts
+++ src/shared/antd/mint/mintSelection/hooks/useJupiterTokenList.ts
@@ -1,25 +1,31 @@
 import { useCallback, useEffect, useState } from 'react'
 import useSWRImmutable from 'swr/immutable'
 
-const fetchJupiterList = async () => {
+const fetchJupiterTokenList = async () => {
   const data = await fetch('https://cache.jup.ag/tokens')
   return data.json()
 }
 
 export const useJupiterTokenList = () => {
-  const [jupiterTokenList, setJupiterTokenList] = useState<string[]>([])
-  const { data } = useSWRImmutable('fetchJpTokenList', fetchJupiterList)
+  const [jupiterTokenList, setJupiterTokenList] = useState<
+    Map<string, boolean>
+  >(new Map())
+  const { data } = useSWRImmutable('fetchJpTokenList', fetchJupiterTokenList)
 
   useEffect(() => {
     if (data) {
-      const formatedTokenList = data.map((token: any) => token.address)
-      setJupiterTokenList(formatedTokenList)
+      const formattedTokenList = new Map<string, boolean>()
+      for (let i = 0; i < data.length; i++) {
+        formattedTokenList.set(data.address, true)
+      }
+
+      setJupiterTokenList(formattedTokenList)
     }
   }, [data])
 
   const verify = useCallback(
     (address: string): boolean => {
-      return jupiterTokenList.includes(address)
+      return jupiterTokenList.has(address)
     },
     [jupiterTokenList],
   )
