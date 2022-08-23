fa1d5f7adf37b6775c828a052c283eed9a871fa8
tuphan-dn <tuphan@descartes.network>
2022-08-23 10:41:38 +0700

    remove unnecessary modules

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,36 +1,10 @@
-import { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useWalletAddress } from '@sentre/senhub'
-
-import { Row, Col, Typography, Button, Space } from 'antd'
-import IonIcon from '@sentre/antd-ionicon'
-
-import { AppDispatch, AppState } from 'model'
-import { increaseCounter } from 'model/main.controller'
+import { Row, Col, Typography } from 'antd'
 
 const View = () => {
-  const address = useWalletAddress()
-  const dispatch = useDispatch<AppDispatch>()
-  const counter = useSelector((state: AppState) => state.main.counter)
-
-  const increase = useCallback(() => dispatch(increaseCounter()), [dispatch])
-
   return (
     <Row gutter={[24, 24]} align="middle">
       <Col span={24}>
-        <Space align="center">
-          <IonIcon name="newspaper-outline" />
-          <Typography.Title level={4}>App View</Typography.Title>
-        </Space>
-      </Col>
-      <Col span={24}>
-        <Typography.Text>Address: {address}</Typography.Text>
-      </Col>
-      <Col>
-        <Typography.Text>Counter: {counter}</Typography.Text>
-      </Col>
-      <Col>
-        <Button onClick={increase}>Increase</Button>
+        <Typography.Title level={4}>Hello</Typography.Title>
       </Col>
     </Row>
   )
